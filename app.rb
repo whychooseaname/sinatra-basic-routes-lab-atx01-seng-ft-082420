490c8a2a3b761b1282ba4168236c523740562977
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-09-14 14:03:30 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Eddie"
+    end
+
+    get '/hometown' do
+        "My hometown is Chino Hills"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Breaking the Habit'"
+    end
 end
